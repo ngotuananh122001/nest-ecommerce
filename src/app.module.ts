bfe2ec6c62f15fa1e24561d49bb6b72ec1a68c47
Tantf <ah.ngotuananh122001@gmail.com>
2023-01-27 15:48:24 +0700

    finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/store'),
-    ProductModule
+    MongooseModule.forRoot('mongodb://mongoadmin:bdung@localhost/store', {
+      authSource: 'admin',
+    }),
+    ProductModule,
+    UserModule,
+    AuthModule,
+    CartModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
+import { MongooseModule } from '@nestjs/mongoose'; // 1. Import mongoose module
+import { ProductSchema } from './schemas/product.schema'; // 2. Import product schema
 
 @Module({
-  providers: [ProductService],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]) // 3. Setup the mongoose module to use the product schema
+  ],
   controllers: [ProductController],
+  providers: [ProductService]
 })
-export class ProductModule {}
+export class ProductModule { }
